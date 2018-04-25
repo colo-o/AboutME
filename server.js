38b9d0200906cf01085a6eca08ac89f6e5216d24
StevenAnaya <stvanaya@hotmail.com>
2018-04-24 22:15:50 -0500

    Create first template with PUG library

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 'use strict'
 
-let express = require('express');
-let server = express();
+let express = require('express')
+let server = express()
 let port = 8080
 
+server.use(express.static('styles'))
+server.set('views', './views')
+server.set('view engine', 'pug')
+
 server.get('/', (req, res) => {
-  res.send('Hello World');
+  res.render('index', { title: 'Mi Aplicacion con PUG', message: 'Hola desde un motor de templates' })
 })
 
 server.get('*', (req, res) => {
-  res.send(404, 'No Encontramos lo que estas buscando');
+  res.status(404).send('<h2 style="color: red;">No Encontramos lo que estas buscando</h2>')
 })
 
 server.listen(port, () => {
-  console.log('Server is running');
+  console.log('Server is running')
 })
