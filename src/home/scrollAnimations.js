db6b6d975f5799b181637cc9a0a7192dc1d1c5c6
StevenAnaya <stvanaya@hotmail.com>
2018-04-29 23:08:45 -0500

    Fixing some issues with animation and adding SVG's files

--- src/home/scrollAnimations.js
+++ src/home/scrollAnimations.js
@@ -1,11 +1,14 @@
 'use strict'
 
+// ScrollAnimations Module, Beware, don't change any value at the animation object 
+
 let controller = new ScrollMagic.Controller()
 
 module.exports = function scene (){
   var wipeAnimation = new TimelineMax()
-    .fromTo("section.slides.graphic", 2, { x: "100% "}, { x: "0%", ease: Linear.easeNone })
-    .fromTo("section.slides.develop", 2, { x: "-100% "}, { x: "0%", ease: Linear.easeNone })
+    .fromTo("section.slides.graphic", 10, { x: "100% "}, { x: "0%", ease: Linear.easeNone, delay: 3 })
+    .fromTo("section.slides.develop", 10, { y: "-100% "}, { y: "0%", ease: Linear.easeNone, delay: 3 })
+    .fromTo("section.slides.gaming", 10, { y: "100% "}, { y: "0%", ease: Linear.easeNone, delay: 3 })
 
   new ScrollMagic.Scene({
     triggerElement: "#scroll-container",
@@ -16,4 +19,14 @@ module.exports = function scene (){
   .setTween(wipeAnimation)
   .addIndicators()
   .addTo(controller)
+
+  // let next = document.querySelector("#next")
+  // next.addEventListener('click', () => {
+  //   let wipeAnimation2 = new TimelineMax()
+  //   .fromTo("section.slides.graphic", 10, { x: "100% "}, { x: "0%", ease: Linear.easeNone, delay: 3 })
+  //   .fromTo("section.slides.develop", 10, { y: "-100% "}, { y: "0%", ease: Linear.easeNone, delay: 3 })
+  //   .fromTo("section.slides.gaming", 10, { y: "100% "}, { y: "0%", ease: Linear.easeNone, delay: 3 })
+  //   .play()
+  // })
+
 }
