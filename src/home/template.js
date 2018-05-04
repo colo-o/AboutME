0b15266c7862039d3f04bf3e9508e17c869d892c
StevenAnaya <stvanaya@hotmail.com>
2018-05-03 22:55:32 -0500

    Finishing CSS Grid Layout

--- src/home/template.js
+++ src/home/template.js
@@ -8,8 +8,7 @@ module.exports = yo`<div class="grid-container">
   <div class="slide-right grid">FLECHA DER</div>
   <div class="central-content grid">CONTENIDO CENTRAL</div>
   <div class="slide-left grid">FLECHA IZQ</div>
-  <div class="social grid">SOCIAL ICONO</div>
-  <div class="langs grid">BANDERAS ICONO</div>
+  <div class="footer grid">SOCIAL ICONO</div>
     <section class="home"></section>
     <section class="graphic"></section>
     <section class="develop"></section>
