8770e8aaa9cc38f9e3a4eb88b885a02d3dbc8344
StevenAnaya <stvanaya@hotmail.com>
2018-05-17 22:50:33 -0500

    opacity slides

--- src/main-route/animations/slides/opacity.js
+++ src/main-route/animations/slides/opacity.js
@@ -1,10 +1,15 @@
 'use strict'
 
 function opacity (currentPosition, targetPosition) {
+  let currentSlide = $(`article.main-content-panel.${currentPosition} div:last-child`)
   if (currentPosition === "home") {
-    let log = $(`article.main-content-panel.${currentPosition} div:last-child`)
-    console.log(log)
+    currentSlide.toggleClass('active')
   }
+  if (currentPosition != "home") {
+    currentSlide.toggleClass('active')
+  }
+  let targetSlide = $(`article.main-content-panel.${targetPosition} div:last-child`)
+  targetSlide.toggleClass('active')
 }
 
 module.exports = opacity
