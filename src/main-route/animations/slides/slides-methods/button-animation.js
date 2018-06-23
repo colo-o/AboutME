294b823587bad26b4c65173fb529c87346d5946c
StevenAnaya <stvanaya@hotmail.com>
2018-06-23 01:48:45 -0500

    oc background

--- src/main-route/animations/slides/slides-methods/button-animation.js
+++ src/main-route/animations/slides/slides-methods/button-animation.js
@@ -3,7 +3,6 @@
 export default function buttonAnimation (isForward) {
 	let $btnObj
 		isForward ? $btnObj = $('#forward') : $btnObj = $('#backward')
-		// $btnObj.hasClass('sb-button-is-clicked') ? $btnObj.toggleClass('sb-button-is-clicked') : false;
 		if ($btnObj.hasClass(`${isForward ? 'sb-button-is-clicked-f' : 'sb-button-is-clicked-b'}`)) {
 			$btnObj.toggleClass(`${isForward ? 'sb-button-is-clicked-f' : 'sb-button-is-clicked-b'}`)
 		}
